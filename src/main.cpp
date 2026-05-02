263fa8df71c35aee7049e81514140643275b56e5
dragonerboom <antonopoulosv2@gmail.com>
2026-05-03 01:50:24 +0300

    Integrated I2C display, visualised joystick to confirm math

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
+#include <U8g2lib.h>
+#include <Wire.h>
 #define x 0
 #define y 1
 //objects:
-LiquidCrystal_I2C lcd(0x27,20,4);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C screen(U8G2_R0, U8X8_PIN_NONE);
 //variables:
 struct Payload{
   double amplitude; //0 - 35
@@ -31,20 +32,16 @@ int get_joy_vals(int stick,int dimension,bool inverted){
 
 void setup() {
   //init screen:
-  lcd.init();
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.clear();
-  lcd.print("init proc");
+  screen.begin();
+  screen.setFont(u8g2_font_t0_11_tr);
+  screen.setContrast(255);
   //init inputs:
   for(int i = 0; i < 2; i++){
     for(int v = 0; v< 2; v++){
       pinMode(joyIndex[i][v],INPUT);
     }
   }
-  lcd.clear();
-  lcd.print("finished setup");
+  
 }
 
 void loop() {
@@ -57,5 +54,20 @@ void loop() {
   data.direction = atan2(get_joy_vals(0,y,0), get_joy_vals(0,x,0));
   data.spin = map(get_joy_vals(1,y,1), -1, 1,-20,20);
   data.level = map(get_joy_vals(1,x,1), -1, 1, 20, -20);
+  //display sstick info on screen
+  screen.clearBuffer();
+  screen.drawCircle(64, 32, 28);
+  screen.drawLine(64, 32, map(get_joy_vals(0, x, 0),-1,1,64-28,64+28),map(get_joy_vals(0, y, 0),-1,1,64-28,64+28));
+  screen.sendBuffer();
+  //demo text
 
+  /*
+  screen.clearBuffer();
+  screen.setCursor(0, 10);                 
+  screen.print("I WORK !!!!");
+  screen.setCursor(0,19);
+  screen.print(":D");
+  screen.sendBuffer();
+  delay(1000);
+  */
 }
