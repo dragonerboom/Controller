96358bdc64577f64e07b2c58b05125d129d17e7f
dragonerboom <antonopoulosv2@gmail.com>
2026-08-08 00:52:13 +0300

    Switch back to esp32 c3, prepared for redesign, changed input handling pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
-#include "esp32-hal.h"
 #include <Arduino.h>
-#include <TFT_eSPI.h>
-#include <Wire.h>
-#include <Adafruit_ADS1X15.h>
 #include <cmath>
 #include <esp_now.h>
 #include <WiFi.h>
@@ -10,7 +6,6 @@
 #define y 1
 //##################################OBJECTS##################################
 esp_now_peer_info_t peerInfo;
-Adafruit_ADS1115 ads;
 //##################################PAYLOAD##################################
 struct Payload{
   double amplitude; //0 - 35
@@ -35,16 +30,16 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
+double deadzone(double stick){
+  if (fabs(stick) < 0.2){return 0;}
+  else{return stick;}
+}
 
 void setup() {
   //init screen:
   
   //init inputs:
-  for(int i = 0; i < 2; i++){
-    for(int v = 0; v< 2; v++){
-      pinMode(joyIndex[i][v],INPUT);
-    }
-  }
+  
   //init Wifi
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
@@ -61,27 +56,28 @@ void setup() {
     return;
   }
   //init ads
-  ads.begin()
-  ads.setGain(GAIN_TWOTHIRDS);
+  
 }
 
 void loop() {
-
-  stickAX = map(ads.readADC_SingleEnded(0),0,26400,-1,1);
-  stickAY = map(ads.readADC_SingleEnded(1),0,26400,-1,1);
-  stickBX = map(ads.readADC_SingleEnded(2),0,26400,-1,1);
-  stickBY = map(ads.readADC_SingleEnded(3),0,26400,-1,1);
-
-  data.amplitude = stickAX;
-  data.direction = atan2(stickAY, stickAX);
-  data.spin = stickBY; 
-  data.level= stickBX;
-
+  //get input
+  stickAX = (float)(analogRead(A0) - 2048) / 2048;
+  stickAY = (float)(analogRead(A1) - 2048) / 2048;
+  stickBX = (float)(analogRead(A2) - 2048) / 2048;
+  stickBY = (float)(analogRead(A3) - 2048) / 2048;
+  //deadzone
+  stickAX = deadzone(stickAX); 
+  stickAY = deadzone(stickAY);
+  stickBX = deadzone(stickBX);
+  stickBY = deadzone(stickBY);
+  
+  //send data
   esp_now_send(receiverAddress, (uint8_t *) &data, sizeof(data));
 
 
-  //print stats to screen
+  
   
   
   delay(1000);
 }
+
